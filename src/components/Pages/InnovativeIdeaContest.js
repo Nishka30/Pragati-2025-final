fd0c8c5c1ccf1e2e50ffa19e0226c07fe7986f18
Supratim Nag <snsupratim@gmail.com>
2025-01-09 16:53:23 +0530

    fixed team members limits

--- src/components/Pages/InnovativeIdeaContest.js
+++ src/components/Pages/InnovativeIdeaContest.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "./css/Inner-Event.css";
 import Background from "./Background";
@@ -10,16 +9,18 @@ const InnovativeIdeaContest = () => {
   const [activeTab, setActiveTab] = useState("About");
 
   const tabsContent = {
-    About: "The guidelines ask for articles in specific topics and a specific format for new ideas. Presentations should be in PowerPoint and include the problem, solution, and why the solution is unique and possible.",
+    About:
+      "The guidelines ask for articles in specific topics and a specific format for new ideas. Presentations should be in PowerPoint and include the problem, solution, and why the solution is unique and possible.",
     Timeline: "",
-    RegistrationFee: "MCKVIE STUDENTS : 100rs/Team & OUTSIDE Students : 200rs/Team.",
-    Contacts: "For any query contact: "
+    RegistrationFee:
+      "MCKVIE STUDENTS : 100rs/Team & OUTSIDE Students : 200rs/Team.",
+    Contacts: "For any query contact: ",
   };
 
   const card = {
     id: 1,
     image: eventImage, // Replace with actual image URL
-    rating: "team : 4-6",
+    rating: "team : 1-6",
     title: "Innovative Idea Contest",
   };
 
@@ -52,9 +53,15 @@ const InnovativeIdeaContest = () => {
             </div>
             <p>{tabsContent[activeTab]}</p>
             <div className="button-group">
-            <a href="https://forms.gle/LdnRcMHx8UmEeawCA"><button className="btn">Register</button></a>
-              <a href="https://drive.google.com/file/d/1wr1miLPm4FNuaMEZjqn1_tcs2OeHr6Vn/view?usp=drive_link"><button className="btn">Rule Book</button></a>
-              <a href="https://drive.google.com/file/d/1CeJkurMsE851m-eSa88T71toqM5iU6sS/view?usp=sharing"><button className="btn">General Rule</button></a>
+              <a href="https://forms.gle/LdnRcMHx8UmEeawCA">
+                <button className="btn">Register</button>
+              </a>
+              <a href="https://drive.google.com/file/d/1wr1miLPm4FNuaMEZjqn1_tcs2OeHr6Vn/view?usp=drive_link">
+                <button className="btn">Rule Book</button>
+              </a>
+              <a href="https://drive.google.com/file/d/1CeJkurMsE851m-eSa88T71toqM5iU6sS/view?usp=sharing">
+                <button className="btn">General Rule</button>
+              </a>
             </div>
           </div>
         </div>
